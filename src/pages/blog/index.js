fdb1ab29c0c20aa2f43992aa1a5a4c2784739960
Sabra <sabravilla@gmail.com>
2019-12-24 11:47:45 -0800

    blogroll pink added

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,37 +1,42 @@
 import React from "react";
-
+import styled from "styled-components";
 import Layout from "../../components/Layout";
 import BlogRoll from "../../components/BlogRoll";
+const BlogIndexSection = styled.section`
+  background-image: linear-gradient(110deg, #9796f0, #fbc7d4);
+`;
 
 export default class BlogIndexPage extends React.Component {
   render() {
     return (
       <Layout>
-        <div
-          className="full-width-image-container margin-top-0"
-          style={{
-            backgroundImage: `url('/img/fish.jpg')`
-          }}
-        >
-          <h1
-            className="has-text-weight-bold is-size-1"
+        <BlogIndexSection>
+          <div
+            className="full-width-image-container margin-top-0"
             style={{
-              boxShadow: "0.5rem 0 0 #f40, -0.5rem 0 0 #f40",
-              backgroundColor: "#f40",
-              color: "white",
-              padding: "1rem"
+              backgroundImage: `url('/img/chicken-2.jpg')`
             }}
           >
-            Latest Stories from Sasha
-          </h1>
-        </div>
-        <section className="section">
-          <div className="container">
-            <div className="content">
-              <BlogRoll />
-            </div>
+            <h1
+              className="has-text-weight-bold is-size-1"
+              style={{
+                boxShadow: "0.5rem 0 0 #f40, -0.5rem 0 0 #f40",
+                backgroundColor: "#f40",
+                color: "white",
+                padding: "1rem"
+              }}
+            >
+              Latest Stories from Sasha
+            </h1>
           </div>
-        </section>
+          <section className="section">
+            <div className="container">
+              <div className="content">
+                <BlogRoll />
+              </div>
+            </div>
+          </section>
+        </BlogIndexSection>
       </Layout>
     );
   }
